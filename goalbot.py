7d705bd51f5573792d1517e957049a1e5dbda840
Colin-Colin <35017473+Colin-Colin@users.noreply.github.com>
2018-01-27 23:22:22 -0600

    Update queries for FTS5 support

--- goalbot.py
+++ goalbot.py
@@ -4,8 +4,11 @@ import sqlite3
 from time import sleep
 from datetime import datetime
 
-REPLY_FOOTER = '''___\n\n^^[About](https://redd.it/7kfrvj)
-^^| ^^[Creator](https://reddit.com/u/MUFColin) ^^| ^^[Feedback](/r/goalbot) ^^| ^^[Donate](https://www.reddit.com/r/goalbot/wiki/donate)'''
+FOOTER = '''___\n\n
+        ^^[About](https://redd.it/7kfrvj)
+        ^^| ^^[Creator](https://reddit.com/u/MUFColin) 
+        ^^| ^^[Feedback](/r/goalbot) 
+        ^^| ^^[Donate](https://www.reddit.com/r/goalbot/wiki/donate)'''
 
 
 def authenticate():
@@ -19,73 +22,88 @@ def authenticate():
 def get_urls(query):
     query = query.split(',')
 
-    if(len(query) < 2 and query[0].strip() != 'random'):
+    if len(query) < 2 and query[0].strip() != 'random':
         return ''
+    ##        x = get_urls(','.join(query[0].split(' ')))
+    ##        if(x):
+    ##            return x
+    ##        else:
+    ##            return ''
+    ##
+    ##        return get_urls(','.join(query[0].split(' ')))
+    # return ''
 
     parameters = []
 
+    # sSQL = '''SELECT GfyID, AltGfy1, AltGfy2, AltGfy3, AltGfy4, Player, Competition, Season FROM Goals
+    #         WHERE PlayerID=(SELECT PlayerID FROM Players
+    #         WHERE UPPER(?) IN (UPPER(DistinctFirst), UPPER(DistinctLast), UPPER(AltName1), UPPER(AltName2), UPPER(FullName)))'''
     sSQL = '''SELECT GfyID, AltGfy1, AltGfy2, AltGfy3, AltGfy4, Player, Competition, Season FROM Goals
-            WHERE PlayerID=(SELECT PlayerID FROM Players
-                            WHERE UPPER(?) IN (UPPER(DistinctFirst), UPPER(DistinctLast), UPPER(AltName1), UPPER(AltName2), UPPER(FullName)))'''
-    
+            WHERE PlayerID=(SELECT PlayerID FROM test_Players
+            WHERE test_Players MATCH (?))'''
+
     player_name = query[0].strip()
     parameters.append(player_name)
 
-    #if(query[1]):
+    # if(query[1]):
     if 0 <= 1 < len(query):
         opponent = query[1].strip()
+        # sSQL += ''' AND MatchID IN (SELECT MatchID FROM Matches WHERE TeamID=(
+        #     SELECT TeamID FROM Teams WHERE UPPER(?) IN (Acronym, UPPER(FullName), UPPER(ShortName1), UPPER(ShortName2), UPPER(ShortName3))))'''
         sSQL += ''' AND MatchID IN (SELECT MatchID FROM Matches WHERE TeamID=(
-            SELECT TeamID FROM Teams WHERE UPPER(?) IN (Acronym, UPPER(FullName), UPPER(ShortName1), UPPER(ShortName2), UPPER(ShortName3))))'''
+                    SELECT TeamID FROM test_Teams WHERE test_Teams MATCH (?)))'''
 
         parameters.append(opponent)
-        
+
     if 0 <= 2 < len(query):
         season = '%' + query[2].strip() + '%'
-        sSQL += ' AND Season LIKE ?'
+        sSQL += ' AND (Season LIKE ?)'
         parameters.append(season)
- 
+
     sSQL += ';'
 
-    if(query[0].strip() == 'random'):
+    if query[0].strip() == 'random':
         sSQL = '''select GfyID, AltGfy1, AltGfy2, AltGfy3,
                 AltGfy4, Player, Competition, Season
                 from goals order by random() limit 3;'''
         parameters = []
-
-    con = sqlite3.connect('bot.db')
-    c = con.cursor()
     
+    con = sqlite3.connect('bot.db')
+    
+    c = con.cursor()
+
     rows = c.execute(sSQL, tuple(parameters))
 
     reply = ''
     for row in rows:
-        #reply += '[{}: {}, {}](https://gfycat.com/{})\n\n'.format(row[1], row[2], row[3], row[0])
+        # reply += '[{}: {}, {}](https://gfycat.com/{})\n\n'.format(row[1], row[2], row[3], row[0])
         reply += '[{}: {} ({})](https://gfycat.com/{})'.format(row[5], row[6], row[7], row[0])
-        for angle in range(1,5):
-            if(row[angle]):
+        for angle in range(1, 5):
+            if row[angle]:
                 reply += ', [Alt{}](https://gfycat.com/{})'.format(angle, row[angle])
         reply += '\n\n'
-        
+
     if not reply:
         return ''
 
-    reply += REPLY_FOOTER
+    reply += FOOTER
     return reply
+
 
 def run_bot(reddit):
     print('Getting comments')
 
     for comment in reddit.subreddit('reddevils+mufcolin+goalbot').stream.comments():
-        #match = re.findall('^!goalbot\s', comment.body)
+        # match = re.findall('^!goalbot\s', comment.body)
         body = comment.body
         start_index = body.find('!goalbot ')
 
         if (start_index != -1):
             print('found comment: {}'.format(comment.permalink))
 
-##            body = body[start_index:]
-##            end_index = body.find('\n')
-            
+            ##            body = body[start_index:]
+            ##            end_index = body.find('\n')
+
             with open('commented.txt', 'r') as outfile:
                 seen_comments = outfile.read().splitlines()
 
@@ -93,22 +111,22 @@ def run_bot(reddit):
                 try:
                     print('new comment')
 
-                    body = body[start_index+9:] #len('!goalbot ') == 9
+                    body = body[start_index + 9:]  # len('!goalbot ') == 9
                     end_index = body.find('\n')
                     if end_index == -1:
                         query = body
                     else:
                         query = body[:end_index]
-                    
+
                     print('query: {}'.format(query))
                     reply = get_urls(query)
 
-                except Exception as e:  #fix
+                except Exception as e:  # fix
                     print(e)
 
                 else:
-                    if(reply != ''):
-                        #print('reply: {}'.format(reply))
+                    if (reply != ''):
+                        # print('reply: {}'.format(reply))
                         try:
                             comment.reply(reply)
                             print('reply made')
@@ -116,14 +134,14 @@ def run_bot(reddit):
                             with open('commented.txt', 'a+') as outfile:
                                 outfile.write(comment.id + '\n')
 
-                            #print('waiting 20 seconds')
+                            # print('waiting 20 seconds')
                             print(str(datetime.now().time()))
                             print('***********************')
                             sleep(20)
-                            
+
                         except praw.exceptions.APIException as api_exception:
                             print(api_exception)
-                            
+
                     else:
                         with open('non-working.txt', 'a+') as errout:
                             try:
@@ -135,22 +153,23 @@ def run_bot(reddit):
             else:
                 print('seen')
 
-
-def main():
-    reddit = authenticate()
-    while True:
-        try:
-            run_bot(reddit)
-        except prawcore.exceptions.ServerError as http_error:
-            print(http_error)
-            print('waiting 2 minutes')  #reduce server load
-            sleep(120)
-        except prawcore.exceptions.ResponseException as response_error:
-            print(response_error)
-            print('waiting 2 minutes')
-            sleep(120)
-        except Exception as e:
-            print('error: {}'.format(e))
-
-if __name__ == '__main__':
-    main()
+# def main():
+#     reddit = authenticate()
+#     while True:
+#         try:
+#             run_bot(reddit)
+#         except prawcore.exceptions.ServerError as http_error:
+#             print(http_error)
+#             print('waiting 2 minutes')  #reduce server load
+#             sleep(120)
+#         except prawcore.exceptions.ResponseException as response_error:
+#             print(response_error)
+#             print('waiting 2 minutes')
+#             sleep(120)
+#         except Exception as e:
+#             print('error: {}'.format(e))
+#             print('waiting 2 minutes')
+#             sleep(120)
+#
+# if __name__ == '__main__':
+#     main()
